c1864950523cbe704fab8569f496933fc363dc85
Pouya <pouyakoupaie.2003@gmail.com>
2026-06-07 15:37:55 +0330

    starter commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans bg-background dark:bg-black">
+      <main className="flex justify-around ">
+       
+      </main>
+    </div>
+  );
+}
